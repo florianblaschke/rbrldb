65fc02efdd6ee94e2b7e14dd51e2498361ce7136
Florian <127996345+florianblaschke@users.noreply.github.com>
2026-04-21 21:53:36 +0200

    add dockerfile

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,8 @@ async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("kv_store".into(), "error".into(), std::io::stdout);
     init_subscriber(subscriber);
 
-    let addr = format!("{}:{}", "127.0.0.1", "7878");
+    let port = std::env::var("PORT").unwrap_or_else(|_| "7878".to_string());
+    let addr = format!("0.0.0.0:{}", port);
     let listener = TcpListener::bind(&addr).await?;
     tracing::info!("starting server on port: {}", &addr);
 
